92834d290b71e3149b10bf21fe633679b6cfe6e1
Michel França <fr.mich17@gmail.com>
2021-09-15 21:51:38 -0300

    Seção 1 concluída

---
+++ Seção 1 - Introdução/basic.js
@@ -0,0 +1,3 @@
+"use strict";
+var helloWorld = 'Hello World in Typescript v.2';
+console.log(helloWorld);
---
+++ Seção 1 - Introdução/basic.ts
@@ -0,0 +1,3 @@
+const helloWorld: string = 'Hello World in Typescript v.2'
+
+console.log(helloWorld)
