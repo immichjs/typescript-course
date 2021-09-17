f4a6070dc787c1fd18328a132026091eb29b0075
Michel França <fr.mich17@gmail.com>
2021-09-17 02:34:06 -0300

    Seção 3 concluída

---
+++ Seção 3 - Compilador/compilador.js
@@ -0,0 +1,18 @@
+"use strict";
+var channel = 'Gaveta';
+var subscribers = 298231;
+// channel = subscribers
+console.log(channel);
+// let nome = 'Mich'
+function soma(a, b) {
+    return a + b;
+}
+var qualquerCoisa;
+qualquerCoisa = 12;
+qualquerCoisa = 'abc';
+function saudar(isManha) {
+    return isManha ? 'Bom dia' : 'Boa Vida';
+}
+console.log(saudar(true));
+console.log(saudar(false));
+//# sourceMappingURL=compilador.js.map
---
+++ Seção 3 - Compilador/compilador.ts
@@ -0,0 +1,24 @@
+let channel: string = 'Gaveta'
+let subscribers: number = 298231
+
+// channel = subscribers
+console.log(channel)
+
+// let nome = 'Mich'
+
+function soma (a: any, b: any) {
+  return a + b
+}
+
+let qualquerCoisa 
+qualquerCoisa = 12
+qualquerCoisa = 'abc'
+
+console.log(soma(1, 2))
+
+function saudar (isManha: boolean): string {
+  return isManha ? 'Bom dia' : 'Boa Vida'
+}
+
+console.log(saudar(true))
+console.log(saudar(false))
