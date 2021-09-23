0a35026defa9b953b66dd8396d60b6093df36a70
Michel França <fr.mich17@gmail.com>
2021-09-23 01:41:41 -0300

    Seção 14 concluída

---
+++ Seção 14 - Integração Vue + TS/integracao-vuejs/src/components/Count.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Contador</h1>
+    <h2>{{ count }}</h2>
+    <button @click="setCount(-1)">-</button>
+    <button @click="setCount(1)">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return { 
+      count: this.initialValue || 0
+    }
+  },
+  methods: {
+    setCount (delta: number) {
+      this.count += delta
+    }
+  },
+  props: {
+    initialValue: { type: Number }
+  }
+})
+</script>
+
+<style>
+
+</style>
---
+++ Seção 14 - Integração Vue + TS/integracao-vuejs/src/components/CountClass.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Contador (Classe)</h1>
+    <h1>{{ count }}</h1>
+    <h2>{{ getParOuImpar() }}</h2>
+    <h2>{{ parOuImpar }}</h2>
+    <button @click="setCount(-1)">-</button>  
+    <button @click="setCount(1)">+</button>  
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class CountClass extends Vue {
+  @Prop(Number) private readonly initialValue?: number // initialValue! = obrigatório
+  private count: number = this.initialValue || 0
+
+  public setCount (delta: number) {
+    this.count += delta
+  }
+
+  public get ParOuImpar (): string {
+    return this.count % 2 === 0 ? 'Par' : 'Impar'
+  }
+}
+
+</script>
+
+
+<style>
+
+</style>
