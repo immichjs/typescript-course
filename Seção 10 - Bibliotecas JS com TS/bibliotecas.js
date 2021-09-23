a55d0a711fc6a7d2f0dfbfeaf479c1aa1d088039
Michel França <fr.mich17@gmail.com>
2021-09-22 21:48:31 -0300

    Seção 10 concluída

---
+++ Seção 10 - Bibliotecas JS com TS/bibliotecas.js
@@ -0,0 +1,5 @@
+"use strict";
+const message = 'Olá mundo';
+const h1 = `<h1>${message}</h1`;
+$('body').append(h1);
+//# sourceMappingURL=bibliotecas.js.map
---
+++ Seção 10 - Bibliotecas JS com TS/bibliotecas.ts
@@ -0,0 +1,4 @@
+
+const message = 'Olá mundo'
+const h1 = `<h1>${message}</h1`
+$('body').append(h1)
